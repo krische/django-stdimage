f8d7b79f5a4a2eb7a956165eac13e6254429dd47
brian <krische@gmail.com>
2014-07-24 21:31:32 -0500

    updated version to 0.6.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-stdimage',
-    version='0.5.10',
+    version='0.6.0',
     description='Django Standarized Image Field',
     author='codingjoe',
     url='https://github.com/codingjoe/django-stdimage',
